a6e1a535dea5d14b57941e4a6396cceed9843f1f
yangxg <zmrenwu@163.com>
2017-09-02 23:09:29 +0800

    add rich editor

--- comments/forms.py
+++ comments/forms.py
@@ -1,12 +1,17 @@
 from django import forms
+from django.utils.translation import ugettext_lazy as _
 
 from django_comments.forms import CommentForm
+from django_comments.forms import COMMENT_MAX_LENGTH
 
 from . import get_model
+from .widgets import SimditorTextarea
 
 
 class BlogCommentForm(CommentForm):
     parent = forms.IntegerField(required=False, widget=forms.HiddenInput)
+    comment = forms.CharField(label=_('Comment'), widget=SimditorTextarea,
+                              max_length=COMMENT_MAX_LENGTH)
 
     def __init__(self, target_object, parent=None, data=None, initial=None, **kwargs):
         self.parent = parent
@@ -15,6 +20,7 @@ class BlogCommentForm(CommentForm):
         initial.update({'parent': self.parent})
         super().__init__(target_object, data=data, initial=initial, **kwargs)
         self.fields['email'].required = False
+        print(self.fields['parent'].value)
 
     def get_comment_model(self):
         return get_model()
