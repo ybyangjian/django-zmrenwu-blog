6f1016ca9a762a7f3d762ab7a3cd3dcb27ffe39e
yangxg <zmrenwu@163.com>
2017-09-04 17:37:14 +0800

    fix: sitemap import error

--- blogproject/urls.py
+++ blogproject/urls.py
@@ -14,13 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf import settings
-from django.conf.urls import url, include
+from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
 
 from blog.feeds import AllPostsRssFeed
-from blog import sitemaps
+from blog.sitemaps import sitemaps
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
